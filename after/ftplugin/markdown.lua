9f0edb215aeb48dc678614dedd1432a8f1654c93
NEX <veperx@icloud.com>
2022-10-25 00:51:25 +0800

    UPDATE

---
+++ after/ftplugin/markdown.lua
@@ -0,0 +1,14 @@
+
+vim.keymap.set( { 'i', 'n', }, '<C-v>', function ()
+    local folder_path = vim.fn.expand("%:p:h") .. "/images/"
+
+    local default_name =  os.date('%F_%H:%M') .. '.png'
+    local images_name = vim.fn.input('Image Name: ', default_name)
+    local images_path = folder_path .. images_name
+
+    vim.fn.system("mkdir ".. folder_path)
+    vim.fn.system("mv ~/Pictures/Screenshots/Screenshot* " .. images_path)
+    -- vim.fn.system('rm ~/Pictures/Screenshots/Screenshot*.png -f')
+    vim.cmd("normal!i<img height=150 src=\"./images/" .. images_name .. "\" />")
+end,
+{ noremap = true, silent = true })
--- after/ftplugin/markdown.vim
+++
@@ -1,7 +0,0 @@
-abbrev ;1 #
-abbrev ;2 ##
-abbrev ;3 ###
-abbrev ;4 ####
-abbrev ;5 #####
-
-abbrev d +
