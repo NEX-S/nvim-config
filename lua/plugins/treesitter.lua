aba8ea062fd6dc700615054851d9afb053172cb1
NEX <veperx@icloud.com>
2022-10-25 19:43:24 +0800

    UPDATE

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,11 +1,17 @@
 
 require "nvim-treesitter.configs".setup {
     sync_install = true,
-    ensure_installed = { 'c', 'lua', 'php', 'markdown', 'python', 'html', 'vim', 'http', 'json', 'java', 'go' },
+    ensure_installed = { 'c', 'lua', 'php', },
     highlight = {
         enable = true,
         additional_vim_regex_highlighting = false,
-        disable = {},
+        disable = function (lang, buf)
+            local max_filesize = 100 * 1024 -- 100 KB
+            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+            if ok and stats and stats.size > max_filesize then
+                return true
+            end
+        end,
     },
     rainbow = {
         enable = true,
